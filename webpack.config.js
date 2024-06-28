353ffbcdbb7363abb9acba73c857ee30edf5b008
kiken-eng <kanyecope@gmail.com>
2024-06-28 12:02:44 +0300

    chore: Add webpack configuration file for bundling and development setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// Generate content of this file by running `npx webpack init`
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  devtool: "source-map",
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
